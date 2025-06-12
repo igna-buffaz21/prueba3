1275633a2e3488ad27d81b8213728c1f7ab3805a
Ignacio <ignaciobuffaz73@gmail.com>
2025-06-11 21:51:02 -0300

    Actulizacion nose cuanto

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Servir archivos estáticos desde dist/
-app.use(express.static(path.join(__dirname, 'dist/prueba-tailwind-angular')));
+// Servir archivos estáticos desde cualquier carpeta en dist/
+app.use(express.static(path.join(__dirname, 'dist')));
 
-// Redirigir todas las rutas al index.html (para SPA)
+// Redirigir todas las rutas al index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/prueba-tailwind-angular/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.listen(port, () => {
